d1e15c0e8f30eea3e520829b7b908c7aeb5b22f4
khushibhambri <bhambrikhushi4@gmail.com>
2021-09-10 20:57:26 +0530

    portfolio added

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+
+// const sbtn=document.getElementById("sub1");
+
+// sbtn.addEventListener("submit",(e)=>{
+//   e.preventDefault();
+//     alert("message sent successfully");
+
+// });
+
+function submitform( e){
+   e.preventDefault();
+    alert("message sent successfully");
+}
